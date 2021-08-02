020172cf7525d344d160a9af3eef8023dc28499b
hetfly <hetfly@gmail.com>
2021-08-02 20:13:35 +0200

    feat: add type CurrencyIconType

--- src/components/CurrencyIcon/index.tsx
+++ src/components/CurrencyIcon/index.tsx
@@ -3,15 +3,13 @@ import React from 'react';
 import { CurrencyKey } from 'constants/currency';
 import { getCurrencyKeyIcon } from 'utils';
 
-function CurrencyIcon({
-  currencyKey,
-  width,
-  height,
-}: {
+type CurrencyIconProps = {
   currencyKey: CurrencyKey;
   width: string | number | undefined;
   height: string | number | undefined;
-}) {
+};
+
+function CurrencyIcon({ currencyKey, width, height }: CurrencyIconProps) {
   const Icon = getCurrencyKeyIcon(currencyKey);
 
   return <Icon width={width} height={height} />;
