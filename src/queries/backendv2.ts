31abdd7e8e5d78a5a44c9d2580e00731090ebf98
hetfly <hetfly@gmail.com>
2023-03-08 22:05:12 +0100

    feat: increase token liquidity limit

--- src/queries/backendv2.ts
+++ src/queries/backendv2.ts
@@ -48,7 +48,7 @@ export const useContractMethodsDataQuery = (
 };
 
 export function useAllTokens() {
-  const MIN_LIQUIDITY_TO_INCLUDE = 9;
+  const MIN_LIQUIDITY_TO_INCLUDE = 500;
 
   return useQuery(
     `allTokens`,
