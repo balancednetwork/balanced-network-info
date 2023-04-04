a72871a75df3b903c51e5834931aebec4b8d00f9
hetfly <hetfly@gmail.com>
2023-04-04 10:48:18 +0200

    fix: add feedback

--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -103,10 +103,11 @@ const Chart = ({
             axisLine={false}
             tickLine={false}
             orientation="right"
-            tick={{ stroke: 'white' }}
+            tick={{ stroke: theme.colors.text1, fontSize: '14px' }}
             tickFormatter={value => formatYAxisNumber(value, value > 100 ? 1 : 2)}
             width={20}
             domain={['auto', 'auto']}
+            padding={{ top: 10, bottom: 25 }}
           />
 
           <Tooltip
