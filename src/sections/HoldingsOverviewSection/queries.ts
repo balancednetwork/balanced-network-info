8f06dd1d6365a6d87e6bad9c318b813d5d5abc40
hetfly <hetfly@gmail.com>
2024-03-07 10:12:46 +0100

    feat: change sicx color to white

--- src/sections/HoldingsOverviewSection/queries.ts
+++ src/sections/HoldingsOverviewSection/queries.ts
@@ -11,8 +11,8 @@ import { CHART_COLORS } from 'sections/BALNSection/queries';
 const daoFundAddress = addresses[1].daofund;
 
 const CHART_TOKENS_COLORS = {
-  // sICX: '#8363E8',
-  sICX: '#776EE6',
+  sICX: '#C4C9D0',
+  // sICX: '#776EE6',
   BALN: '#3F7D92',
   bnUSD: '#66C7A6',
   BTCB: '#E9983D',
